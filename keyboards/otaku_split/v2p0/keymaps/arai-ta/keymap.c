48925523d61b225c4c4cd9310b0e5c5ced684fa3
ARAI Takashi <takashi.arai.nz@icloud.com>
2021-05-10 01:03:33 +0900

    カーソルレイヤーを移植した

--- keyboards/otaku_split/v2p0/keymaps/arai-ta/keymap.c
+++ keyboards/otaku_split/v2p0/keymaps/arai-ta/keymap.c
@@ -30,10 +30,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 			MO(CURSOR),KC_LALT,KC_LGUI,LGUI_T(KC_LANG2),KC_BSPC,KC_ENT,KC_SPC,      LT(CURSOR,KC_SPC),KC_ENT,KC_BSPC,KC_LANG1,KC_RGUI,KC_RALT,KC_PGDN,KC_PGUP
   ),
   [CURSOR] = LAYOUT(
-			KC_ESC, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,             KC_F6, KC_F7,  KC_F8,  KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_JYEN,KC_DEL,
-			KC_TAB,   KC_Q,  KC_UP,  KC_E,  KC_R,  KC_T,  KC_Y,      KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_LBRC,KC_RBRC,KC_PSCR,
-			KC_ESC,  KC_LEFT,  KC_DOWN,  KC_RIGHT,  KC_F,  KC_G,  KC_H,              KC_LEFT,  KC_DOWN,  KC_UP,  KC_RIGHT,  KC_SCLN,  KC_QUOT,KC_NUHS,
-			KC_LSHIFT,KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,             KC_B,  KC_N,  KC_M,  KC_COMM, KC_DOT, KC_END,KC_UP,KC_HOME,
-			KC_LCTRL, KC_LGUI,KC_LALT,KC_MHEN,KC_TRNS,KC_TRNS,KC_TRNS,        KC_TRNS,KC_TRNS,KC_TRNS,KC_HENK,KC_TRNS,KC_LEFT,KC_DOWN,KC_RIGHT
+			KC_F1,   LGUI(KC_UP), KC_F2, KC_F3, KC_F4,   KC_F5,  KC_F6,             KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12,   KC_NO,   _______,
+			_______, KC_NO,   LALT(KC_RGHT), KC_NO, KC_NO, KC_NO, KC_NO,            KC_NO,   KC_PGUP, KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,    _______,
+			_______, KC_NO,   KC_NO,   KC_PGDN, KC_NO,   LGUI(KC_DOWN), KC_NO,      KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,  KC_NO, KC_NO,  KC_NO,
+			_______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_NO,                     KC_NO,   KC_NO,   KC_NO,   KC_VOLD, KC_VOLU, KC_NO,  KC_NO,    _______,
+			_______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______
   )
 };
