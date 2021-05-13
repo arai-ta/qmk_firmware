41dbd2bb7c90c94185fc98adda20534368df26b0
ARAI Takashi <takashi.arai.nz@icloud.com>
2021-05-14 01:22:49 +0900

    create FUNCTION layer

--- keyboards/otaku_split/v2p0/keymaps/arai-ta/keymap.c
+++ keyboards/otaku_split/v2p0/keymaps/arai-ta/keymap.c
@@ -18,7 +18,8 @@
 
 enum layers{
 	BASE=0,
-	CURSOR
+	CURSOR,
+	FUNCTION
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -27,13 +28,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 			KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_ENT,
 			KC_LCTRL,KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS,
 			KC_LSHIFT,KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,                      KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLASH,KC_RO,   KC_RSHIFT,
-			MO(CURSOR),KC_LALT,KC_LGUI,KC_LGUI, KC_LANG2,KC_ENT,  KC_SPC,   LT(CURSOR,KC_BSPC),KC_ENT,KC_LANG1,KC_SPC,  KC_RGUI, KC_RALT, KC_PGDN, KC_PGUP
+			LT(FUNCTION,KC_BSPC),KC_LALT,KC_LGUI,KC_LGUI, KC_LANG2,KC_ENT,  KC_SPC,   LT(CURSOR,KC_BSPC),KC_ENT,KC_LANG1,KC_SPC,  KC_RGUI, KC_RALT, KC_PGDN, KC_PGUP
   ),
   [CURSOR] = LAYOUT(
 			KC_F1,   LGUI(KC_UP), KC_F2, KC_F3, KC_F4,   KC_F5,  KC_F6,             KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NO,   _______,
-			_______, KC_NO,   LALT(KC_RGHT), KC_NO, KC_NO, KC_NO, KC_NO,            KC_NO,   KC_PGUP, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   _______,
+			_______, KC_NO,   LALT(KC_RGHT), KC_NO, KC_NO, KC_NO, KC_NO,            KC_NO,   KC_PGUP, LGUI(KC_NUHS),LGUI(KC_RBRC), KC_NO,  KC_NO,   KC_NO,   _______,
 			_______, KC_NO,   KC_NO,   KC_PGDN, KC_NO,   LGUI(KC_DOWN), KC_NO,      KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,  KC_NO, KC_NO,   KC_NO,
 			_______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_NO,                     KC_NO,   KC_NO,   KC_NO,   KC_VOLD, KC_VOLU, KC_NO,   KC_NO,   _______,
 			_______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______
+  ),
+  [FUNCTION] = LAYOUT(
+			_______, _______, _______, _______, _______, _______, _______,              _______, _______, _______, _______, _______, _______, _______, _______, _______,
+			_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, _______,              _______, _______, _______, _______, _______, _______, _______, _______,
+			_______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, _______,              _______, _______, _______, _______, _______, _______, _______,
+			_______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,                       _______, _______, _______, _______, _______, _______, _______, _______,
+			_______, _______, _______, _______, _______, _______, LCTL(LGUI(KC_SPC)),   _______, _______, _______, _______, _______, _______, _______, _______
   )
 };
