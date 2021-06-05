dbe7f4c717f3ca6a8df857c1dfa736a187ca877d
ARAI Takashi <takashi.arai.nz@icloud.com>
2021-06-05 17:44:59 +0900

    レイアウトにキーマップを割り付ける

--- keyboards/stte0n/keymaps/default/keymap.c
+++ keyboards/stte0n/keymaps/default/keymap.c
@@ -17,8 +17,7 @@
 
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
-    _BASE,
-    _FN
+    _BASE
 };
 
 // Defines the keycodes used by our macros in process_record_user
@@ -30,10 +29,10 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
-        QMKBEST
-    ),
-    [_FN] = LAYOUT(
-        QMKBEST
+        KC_A, KC_B, KC_C, KC_D,
+        KC_E, KC_F, KC_G, KC_H,
+        KC_I, KC_J, KC_K, KC_L,
+        KC_M, KC_N, KC_O, KC_P
     )
 };
 
