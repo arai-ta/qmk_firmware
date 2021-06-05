e8f708436f8e72647e2b90c73aa00c56ddebe085
ARAI Takashi <takashi.arai.nz@icloud.com>
2021-06-05 20:32:35 +0900

    RGB制御キーをキーマップに定義

--- keyboards/stte0n/keymaps/default/keymap.c
+++ keyboards/stte0n/keymaps/default/keymap.c
@@ -31,8 +31,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
         KC_A, KC_B, KC_C, KC_D,
         KC_E, KC_F, KC_G, KC_H,
-        KC_I, KC_J, KC_K, KC_L,
-        KC_M, KC_N, KC_O, KC_P
+        KC_I, KC_J, KC_K, RGB_TOG,
+        KC_M, RGB_MOD, KC_O, RGB_HUI
     )
 };
 
