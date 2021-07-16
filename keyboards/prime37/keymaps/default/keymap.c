e37c4b2ea2b85ecc1857ab723aac0ef526fa08a1
ARAI Takashi <takashi.arai.nz@icloud.com>
2021-07-17 00:31:26 +0900

    debugging keymap

--- keyboards/prime37/keymaps/default/keymap.c
+++ keyboards/prime37/keymaps/default/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_A,  KC_B,  KC_C,  KC_D,  KC_E,  KC_F,  KC_G,  KC_H,  KC_I,  KC_J,
         KC_0,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,
         KC_Z,  KC_Y,  KC_X,  KC_W,  KC_V,  KC_U,  KC_T,  KC_S,  KC_R,  KC_Q,
-        KC_NO, KC_NO, KC_NO, KC_TAB,  KC_SPC,  KC_ENT,  KC_DEL, KC_NO, KC_NO, KC_NO
+        KC_0 , KC_1 , KC_2 , KC_TAB,KC_SPC,KC_ENT,KC_3 , KC_4 , KC_5 , KC_6 
     )
 };
 
