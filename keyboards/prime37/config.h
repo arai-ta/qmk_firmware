516d44fdf128b6f7046438e7f06462377f1e36e4
ARAI Takashi <takashi.arai.nz@icloud.com>
2021-07-17 01:38:32 +0900

    11列にした

--- keyboards/prime37/config.h
+++ keyboards/prime37/config.h
@@ -41,7 +41,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 #define MATRIX_ROW_PINS { F6, F7, B1, B3 }
-#define MATRIX_COL_PINS { D1, D0, D4, C6, D7, E6, B4, B5, F4, F5 }
+#define MATRIX_COL_PINS { D1, D0, D4, C6, D7, E6, B4, B5, F4, F5, B6 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL */
--- keyboards/prime37/keymaps/default/keymap.c
+++ keyboards/prime37/keymaps/default/keymap.c
@@ -29,10 +29,10 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
-        KC_A,  KC_B,  KC_C,  KC_D,  KC_E,  KC_F,  KC_G,  KC_H,  KC_I,  KC_J,
-        KC_0,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,
-        KC_Z,  KC_Y,  KC_X,  KC_W,  KC_V,  KC_U,  KC_T,  KC_S,  KC_R,  KC_Q,
-        KC_NO, KC_NO, KC_NO, KC_TAB,KC_SPC,KC_ENT,KC_DEL, KC_NO, KC_NO, KC_NO
+        KC_A,  KC_B,  KC_C,  KC_D,  KC_E,  KC_F,  KC_G,  KC_H,  KC_I,  KC_J,  KC_K,
+        KC_0,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9, KC_MINUS,
+        KC_Z,  KC_Y,  KC_X,  KC_W,  KC_V,  KC_U,  KC_T,  KC_S,  KC_R,  KC_Q,  KC_P,
+        KC_NO, KC_NO, KC_NO, KC_TAB,KC_SPC,KC_ENT,KC_DEL, KC_NO, KC_NO, KC_NO, KC_NO
     )
 };
 
