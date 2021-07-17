93a3822a63e2e0b444f9e2d062704512f684ecaf
ARAI Takashi <takashi.arai.nz@icloud.com>
2021-07-17 13:59:01 +0900

    normal keymap

--- keyboards/prime37/keymaps/default/keymap.c
+++ keyboards/prime37/keymaps/default/keymap.c
@@ -17,7 +17,12 @@
 
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
-    _BASE
+    _BASE,
+    /*
+    CURSOR,
+    SYMBOL,
+    META
+    */
 };
 
 // Defines the keycodes used by our macros in process_record_user
@@ -29,10 +34,10 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
-        KC_A,  KC_B,  KC_C,  KC_D,  KC_E,  KC_F,  KC_G,  KC_H,  KC_I,  KC_J,  KC_K,
-        KC_0,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9, KC_MINUS,
-        KC_Z,  KC_Y,  KC_X,  KC_W,  KC_V,  KC_U,  KC_T,  KC_S,  KC_R,  KC_Q,  KC_P,
-        KC_NO, KC_NO, KC_NO, KC_TAB,KC_SPC,KC_ENT,KC_DEL, KC_NO, KC_NO, KC_NO, KC_NO
+        KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_MINUS,
+        KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_ENTER,
+        KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSHIFT,
+        XXXXXXX,XXXXXXX,XXXXXXX,KC_SPC, KC_LGUI,KC_RCTL,KC_BSPC,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX
     )
 };
 
