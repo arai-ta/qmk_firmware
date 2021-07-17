2c155607d1f1222238089f905f1e6a5739816fff
ARAI Takashi <takashi.arai.nz@icloud.com>
2021-07-17 23:53:09 +0900

    add symbol

--- keyboards/prime37/keymaps/default/keymap.c
+++ keyboards/prime37/keymaps/default/keymap.c
@@ -19,8 +19,8 @@
 enum layer_names {
     _BASE,
     CURSOR,
-    /*
     SYMBOL,
+    /*
     META
     */
 };
@@ -38,13 +38,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_ENTER,
         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSHIFT,
         XXXXXXX,XXXXXXX,XXXXXXX,
-                            KC_LGUI, KC_SPC, LT(CURSOR,KC_BSPC), KC_RCTL,
+                       KC_LGUI, LT(SYMBOL,KC_SPC), LT(CURSOR,KC_BSPC), KC_RCTL,
                                                                 XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX
     ),
     [CURSOR] = LAYOUT(
         KC_ESC, LALT(KC_RGHT), LALT(KC_LEFT), KC_NO,   KC_NO,   KC_NO,   KC_PGUP, LGUI(KC_NUHS),LGUI(KC_RBRC), KC_NO,   KC_NO,
         KC_TAB, KC_NO,  KC_PGDN,KC_NO,  LGUI(KC_DOWN),  KC_LEFT,KC_DOWN,KC_UP,  KC_RIGHT,  KC_SCLN,KC_ENTER,
         KC_LSHIFT,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_LANG1,  KC_NO,  KC_VOLD,KC_VOLU,KC_NO,  KC_RSHIFT,
+        XXXXXXX,XXXXXXX,XXXXXXX,
+                                _______,_______,_______,_______,
+                                                                XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX
+    ),
+    [SYMBOL] = LAYOUT(
+        KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_LBRACKET,
+        KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_RBRACKET,
+        KC_Z,   KC_X,   KC_C,   KC_V,   KC_GRV, KC_TILD,KC_EQL, KC_PLUS,KC_BSLS,KC_PIPE,KC_QUOTE,
         XXXXXXX,XXXXXXX,XXXXXXX,
                                 _______,_______,_______,_______,
                                                                 XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX
