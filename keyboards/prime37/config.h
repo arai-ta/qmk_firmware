256230d05ce34e3a80261cfdfe4352b6bd604f3e
ARAI Takashi <takashi.arai.nz@icloud.com>
2021-08-01 00:10:29 +0900

    use B2 pin

--- keyboards/prime37/config.h
+++ keyboards/prime37/config.h
@@ -58,7 +58,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define LED_COMPOSE_PIN B3
 //#define LED_KANA_PIN B4
 
-#define BACKLIGHT_PIN D7
+#define BACKLIGHT_PIN B2
 #define BACKLIGHT_LEVELS 7
 #define BACKLIGHT_BREATHING
 
