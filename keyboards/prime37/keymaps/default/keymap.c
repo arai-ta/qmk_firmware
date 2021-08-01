6ed0bc1876d0abbdafd57ac9fd4bb7fa0b2d84ad
ARAI Takashi <takashi.arai.nz@icloud.com>
2021-08-02 00:02:11 +0900

    re-layout enter control

--- keyboards/prime37/keymaps/default/keymap.c
+++ keyboards/prime37/keymaps/default/keymap.c
@@ -32,10 +32,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
         KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_MINUS,
-        KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_ENTER,
-LSFT_T(KC_Z),   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSHIFT,
+LCTL_T(KC_A),   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,RSFT_T(KC_BSPC),
+LSFT_T(KC_Z),   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_ENTER,
         XXXXXXX,XXXXXXX,XXXXXXX,
-                       KC_LGUI, LT(SYMBOL,KC_SPC), LT(CURSOR,KC_BSPC), KC_RCTL,
+                       KC_LGUI, KC_LCTL, LT(CURSOR,KC_SPC), MO(SYMBOL),
                                                                 XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX
     ),
     [CURSOR] = LAYOUT(
